5ce9a29c64a478169e7b0c0944e2fae3dd02e006
DmitrievaKseniya <dkn1933@mail.ru>
2023-09-13 00:23:36 +1000

    Добавьте файлы проекта.

---
+++ AwesomeNetwork/Data/Repository/FriendsRepository.cs
@@ -0,0 +1,57 @@
+﻿using AwesomeNetwork.Models.Users;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AwesomeNetwork.Data.Repository
+{
+    public class FriendsRepository : Repository<Friend>
+    {
+        public FriendsRepository(ApplicationDbContext db)
+      : base(db)
+        {
+            
+        }
+        
+
+        public async Task AddFriend (User target, User Friend)
+        {
+            var friends = Set.AsEnumerable().FirstOrDefault(x => x.UserId == target.Id && x.CurrentFriendId == Friend.Id);
+
+            if (friends == null)
+            {
+                var item = new Friend()
+                {
+                    UserId = target.Id,
+                    User = target,
+                    CurrentFriend = Friend,
+                    CurrentFriendId = Friend.Id,
+                };
+
+                await Create(item);
+            }
+        }
+
+        public async Task<List<User>> GetFriendsByUser(User target)
+        {
+            var friends = Set.Include(x => x.User).Include(x => x.CurrentFriend).Where(x => x.User.Id == target.Id).Select(x => x.CurrentFriend);
+
+            return await friends.ToListAsync();
+        }
+
+
+        public async Task DeleteFriend(User target, User Friend)
+        {
+            var friends = Set.FirstOrDefault(x => x.UserId == target.Id && x.CurrentFriendId == Friend.Id);
+
+            if (friends != null)
+            {
+                await Delete(friends);
+            }
+        }
+
+    }
+}
